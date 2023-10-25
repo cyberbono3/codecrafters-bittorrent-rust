c5b3fe22b7e8a8a2bbf1a80eff0793f42ed1fb09
Andrei Ivasko <andrei@fantom.foundation>
2023-10-25 19:34:02 +0300

    Parse ints

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,13 @@ use std::env;
 #[allow(dead_code)]
 fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
     // If encoded_value starts with a digit, it's a number
+    if let Some(rest) = encoded_value.strip_prefix('i') {
+        if let Some((digits, _)) = rest.split_once('i') {
+            if let Ok(n) = digits.parse::<i64>() {
+                return n.into()
+            }
+        }
+    }
     if let Some((len, rest)) = encoded_value.split_once(':') {
         if let Ok(len) = len.parse::<usize>() {
             return serde_json::Value::String(rest[..len].to_string());
